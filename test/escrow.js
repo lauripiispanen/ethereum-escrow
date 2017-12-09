4ddb74f0e17d87ea0e1b738ecf598969755ce902
Lauri Piispanen <lauri.piispanen@reaktor.com>
2017-12-09 12:42:37 +0200

    use modern JS syntax

--- test/escrow.js
+++ test/escrow.js
@@ -1,12 +1,12 @@
-const Escrow = artifacts.require("Escrow");
+const Escrow = artifacts.require("Escrow")
 
-contract('Escrow', function(accounts) {
-  it("gets deployed", async function() {
-    var instance = await Escrow.deployed()
-    assert.notEqual(instance, null, "Contract was null");
-  });
-  it("allows deposits", async function() {
-    var instance = await Escrow.deployed()
+contract('Escrow', (accounts) => {
+  it("gets deployed", async () => {
+    const instance = await Escrow.deployed()
+    assert.notEqual(instance, null, "Contract was null")
+  })
+  it("allows deposits", async () => {
+    const instance = await Escrow.deployed()
     await instance.deposit.sendTransaction(accounts[1], { from: accounts[0], value: 100 })
 
     assert.ok(await instance.hasDeposit(accounts[0], accounts[1], 100), "deposit not found")
