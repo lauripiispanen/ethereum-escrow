efafe46c3790f2328e3290819c5fee59d373c610
Lauri Piispanen <lauri.piispanen@reaktor.com>
2017-12-09 10:05:39 +0200

    can store deposits

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,14 +1,33 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.19;
 
 contract Escrow {
-  address public owner;
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
+  struct Deposit {
+    uint    amount;
+    address sender;
+    address recipient;
   }
 
-  function Migrations() public {
+  address public owner;
+
+  mapping (bytes32 => Deposit) deposits;
+
+  function Escrow() public {
     owner = msg.sender;
+  }
+
+  function deposit(address _recipient) public payable {
+    var id = keccak256(msg.sender, _recipient, msg.value);
+    var d = deposits[id];
+    d.recipient = _recipient;
+    d.amount = msg.value;
+    d.sender = msg.sender;
+  }
+
+  function hasDeposit(address _sender, address _recipient, uint _amount) public view returns (bool) {
+    var id = keccak256(msg.sender, _recipient, _amount);
+    var d = deposits[id];
+    return d.amount == _amount && d.sender == _sender && d.recipient == _recipient;
   }
 
 }
--- test/escrow.js
+++ test/escrow.js
@@ -1,9 +1,15 @@
-var Escrow = artifacts.require("Escrow");
+const Escrow = artifacts.require("Escrow");
 
 contract('Escrow', function(accounts) {
-  it("gets deployed", function() {
-    return Escrow.deployed().then(function(instance) {
-      assert.notEqual(instance, null, "Contract wasn't null");
-    });
+  it("gets deployed", async function() {
+    var instance = await Escrow.deployed()
+    assert.notEqual(instance, null, "Contract was null");
   });
+  it("allows deposits", async function() {
+    var instance = await Escrow.deployed()
+    await instance.deposit.sendTransaction(accounts[1], { from: accounts[0], value: 100 })
+
+    assert.ok(await instance.hasDeposit(accounts[0], accounts[1], 100), "deposit not found")
+    assert.ok(!(await instance.hasDeposit(accounts[0], accounts[2], 100)), "deposit should not exist")
+  })
 })
